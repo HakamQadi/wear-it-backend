d1fa0059b9b613c869ddfba112aa2541c45b24be
Hakam Qadi <68652903+HakamQadi@users.noreply.github.com>
2026-08-28 19:08:50 +0300

    qa: stage billing validation

--- src/looks/looks.module.ts
+++ src/looks/looks.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BillingModule } from '../billing/billing.module';
 import { PhotosModule } from '../photos/photos.module';
 import { TryOnModule } from '../try-on/try-on.module';
 import { UploadsModule } from '../uploads/uploads.module';
@@ -7,17 +8,5 @@ import { WardrobeModule } from '../wardrobe/wardrobe.module';
 import { Look, LookSchema } from './look.schema';
 import { LooksController } from './looks.controller';
 import { LooksService } from './looks.service';
-
-@Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Look.name, schema: LookSchema }]),
-    WardrobeModule,
-    PhotosModule,
-    TryOnModule,
-    UploadsModule,
-  ],
-  controllers: [LooksController],
-  providers: [LooksService],
-  exports: [MongooseModule],
-})
+@Module({ imports: [MongooseModule.forFeature([{ name: Look.name, schema: LookSchema }]), WardrobeModule, PhotosModule, TryOnModule, UploadsModule, BillingModule], controllers: [LooksController], providers: [LooksService], exports: [MongooseModule] })
 export class LooksModule {}
